08869c974011720a1431f435bc958d276542e7f2
ngtolstova <124882480+ngtolstova@users.noreply.github.com>
2023-05-27 00:33:00 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -94,7 +94,7 @@ template <typename T>
     }
     int search(const char *look) {
       struct Word wd;
-      std::size_t  L= strlen(look);
+      std::size_t L = strlen(look);
       wd.len = L;
       for (int i = 0; i < L; i++)
         wd.word[i] = *(look+i);
