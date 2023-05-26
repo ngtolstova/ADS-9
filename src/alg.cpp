a857b179a90df12f15cad101b048f3a64f63affc
ngtolstova <124882480+ngtolstova@users.noreply.github.com>
2023-05-26 21:28:38 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,5 @@
 // Copyright 2021 NNTU-CS
-#include  <iostream>
-#include  <fstream>
-#include  <locale>
-#include  <cstdlib>
 #include  "bst.h"
-
 BST<std::string> makeTree(const char* filename) {
   struct Word buf;
   BST <std::string> tree;
