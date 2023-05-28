614202447f75c9e9c318fc63b638465743d063ad
ngtolstova <124882480+ngtolstova@users.noreply.github.com>
2023-05-28 18:10:10 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,27 +1,32 @@
 // Copyright 2021 NNTU-CS
-#include "bst.h"
-BST<std::string> makeTree(const char* filename) {
+#include " bst.h "
+BST<std::string> makeTree (const char* filename) {
   struct Word buf;
-  BST <std::string> tree;
-  std::fstream file;
-  file.open(filename);
-  if (!file) {
+  BST<std::string> tree;
+  fstream file;
+  file.open (filename);
+  if (! file) {
+    cout << "can't open file!\n" << endl;
     return tree;
   }
   if (tree.root == nullptr) {
-    tree.readWord(&file, &buf);
-    if (buf.len == 0) {
+    buf.wd = tree.readWord (&file);
+    if (buf.wd.length () == 0) {
+      cout << "tree empty!\n" << endl;
       return tree;
     }
-    tree.root = new Node{buf, 1, nullptr, nullptr, 1};
-    tree.setD(1);
+    tree.root = new Node { buf, nullptr, nullptr };
+    tree.root->wd.h = 1;
+    tree.root->wd.freq = 1;
+    tree.setD (1);
   }
   while (1) {
-    tree.readWord(&file, &buf);
-    if (buf.len == 0) {
+    buf.wd = tree.readWord (&file);
+    if (buf.wd.length () == 0) {
+      cout << "the tree is ready!\n" << endl;
       break;
     } else {
-      tree.insert(tree.root, buf);
+      tree.insert (tree.root, buf);
     }
   }
   tree.unit = tree.root;
