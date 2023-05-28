1e932258498d6f36e868fd77f0f90f507ba83130
ngtolstova <124882480+ngtolstova@users.noreply.github.com>
2023-05-28 19:33:39 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,13 +4,13 @@ BST<std::string> makeTree(const char* filename) {
   struct Word buf;
   BST<std::string> tree;
   fstream file;
-  file.open (filename);
+  file.open(filename);
   if (!file) {
     cout << "can't open file!\n" << endl;
     return tree;
   }
   if (tree.root == nullptr) {
-    buf.wd = tree.readWord (&file);
+    buf.wd = tree.readWord(&file);
     if (buf.wd.length() == 0) {
       cout << "tree empty!\n" << endl;
       return tree;
@@ -21,12 +21,12 @@ BST<std::string> makeTree(const char* filename) {
     tree.setD(1);
   }
   while (1) {
-    buf.wd = tree.readWord (&file);
+    buf.wd = tree.readWord(&file);
     if (buf.wd.length() == 0) {
       cout << "the tree is ready!\n" << endl;
       break;
     } else {
-      tree.insert (tree.root, buf);
+      tree.insert(tree.root, buf);
     }
   }
   tree.unit = tree.root;
